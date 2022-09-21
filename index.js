2b7ddf5d1031812eaec02192aa38f375f0619fee
Jaspreet Kaur <kaurjaspreet9795@gmail.com>
2022-09-20 23:36:37 -0700

    commit tree b4db76203b333416afc56602178f207df150e4d7

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let main = document.getElementById("main");
+main.remove();
+
+let newHeader= document.createElement("h1");
+newHeader.id='victory';
+// document.body.apend('newHeader');
+newHeader.textContent="Lion is the champion";
